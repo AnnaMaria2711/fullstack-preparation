a23d2450e287457cae91c5a38170daabd44b50dd
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2024-04-22 13:47:36 +0200

    Added User Entity(missing functionality)

---
+++ backend/src/main/java/com/example/backend/model/dao/User.java
@@ -0,0 +1,21 @@
+package com.example.backend.model.dao;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    private Set<Studyset> studysets;
+
+
+}
---
+++ backend/src/main/java/com/example/backend/model/dto/UserCreateRequest.java
@@ -0,0 +1,13 @@
+package com.example.backend.model.dto;
+
+import com.example.backend.model.dao.Studyset;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class UserCreateRequest {
+
+    private String name;
+    private Set<Studyset> studysetSet;
+}

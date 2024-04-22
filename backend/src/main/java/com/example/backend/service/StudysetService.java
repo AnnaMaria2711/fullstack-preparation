9f2e2c76440de9454ce23a4b829bc833912e437d
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2024-04-22 13:47:04 +0200

    Added Studyset Service

---
+++ backend/src/main/java/com/example/backend/service/StudysetService.java
@@ -0,0 +1,27 @@
+package com.example.backend.service;
+
+import com.example.backend.model.dao.Studyset;
+import com.example.backend.model.dto.StudysetCreateRequest;
+import com.example.backend.model.repository.StudysetRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StudysetService {
+
+    private final StudysetRepository studysetRepository;
+
+    public List<Studyset> fetchALLStudysets() {
+        return studysetRepository.findAll();
+    }
+
+
+    public Studyset createStudyset(StudysetCreateRequest request) {
+        Studyset studyset = new Studyset();
+        studyset.setName(request.getName());
+        return studysetRepository.save(studyset);
+    }
+}

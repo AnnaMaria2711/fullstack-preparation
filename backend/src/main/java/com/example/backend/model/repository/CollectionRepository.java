8a34c3056e3ccaa8fb2ab4a6174529dc49401838
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2024-05-08 17:29:38 +0200

    added backend collection elements

--- backend/src/main/java/com/example/backend/model/repository/CollectionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.model.repository;
-
-
-import com.example.backend.model.dao.Collection;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CollectionRepository extends JpaRepository<Collection, Long> {
-
-}

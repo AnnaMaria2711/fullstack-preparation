9e57967b67e1f6a4b1b999fc670ad469e058db0e
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2024-05-08 17:35:00 +0200

    changed code in files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
 import Login from "./Login/Login.tsx";
+import Collections from "./collections/Collections.tsx";
+import AddStudyset from "./addStudyset/AddStudyset.tsx";
 
 function App() {
 
     return (
 
-        <div>
-            <Routes>
-                <Route path="/login" element={<Login/>}/>
-            </Routes>
-        </div>
+        <Routes>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/collections" element={<Collections/>}/>
+            <Route path="/add+studyset" element={<AddStudyset/>}/>
+        </Routes>
 
     )
 

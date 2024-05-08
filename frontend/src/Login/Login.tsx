f5994f93abfd9b0043d0369f070a31fb442e85d5
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2024-05-08 17:34:33 +0200

    added code to Login.tsx and .css

--- frontend/src/Login/Login.tsx
+++ frontend/src/Login/Login.tsx
@@ -1,23 +1,33 @@
 import React, {useState} from 'react';
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
+import "./Login.css";
 
 function Login() {
+    const navigate = useNavigate();
 
     const [name, setName] = useState<string>('');
 
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        axios.get("/user/" + name).then(console.log)
+        axios.get("/user/" + name).then((res) => {
+            console.log(res)
+            localStorage.setItem("User", JSON.stringify(res.data));
+            navigate("/collections");
+        })
+
     }
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <input placeholder={"Enter your Username"} value={name} name="name" type="text" onChange={(e) => {
-                    setName(e.target.value);
-                }}/>
-                <button type="submit">Log in</button>
-            </form>
+            <div className={"center-login"}>
+                <form onSubmit={handleSubmit}>
+                    <input placeholder={"Enter your Username"} value={name} type="text" onChange={(e) => {
+                        setName(e.target.value);
+                    }}/>
+                    <button type="submit">Log in</button>
+                </form>
+            </div>
         </>
     )
 }

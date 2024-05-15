2c721f12148713b85d28cdef9edec08aa4ff8be7
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2024-05-15 17:46:14 +0200

    modified AddStudyset.tsx and CardComponent.tsx

--- frontend/src/addStudyset/AddStudyset.tsx
+++ frontend/src/addStudyset/AddStudyset.tsx
@@ -2,28 +2,52 @@ import React, {useState} from "react";
 import {Link, useNavigate} from "react-router-dom";
 import "./AddStudyset.css";
 import CardComponent from "./CardComponent.tsx";
+import axios from "axios";
+import {Card} from "../types.tsx";
 
 
 function AddStudyset() {
 
     const navigate = useNavigate();
     const [name, setName] = useState<string>("");
+    const [cards] = useState<Card[]>([]);
     const [fields, setFields] = useState<number[]>([1]);
 
-    /* const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-         e.preventDefault();
-         if (!solution || !word || !name) {
-             throw new Error("Input fields must be filled");
-         }
-         if (word && solution !== "") {
-             return
-         }
-         axios.post("/studyset/create" + name + word + solution).then(res => {
-             console.log(res);
-             navigate("/collections");
-         })
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!fields) {
+            throw new Error("Input fields must be filled");
+        }
+        console.log(e.currentTarget);
+        const formData = new FormData(e.currentTarget);
+        console.log(formData);
+        console.log(formData.get("name"))
+        console.log(formData.getAll("word"))
+        console.log(formData.getAll("solution"))
+        const words = formData.getAll("word");
+        const solutions = formData.getAll("solution");
+        words.filter((word, index) => word !== "" && solutions[index] !== "")
+            .map((word, index) => {
+                cards.push({
+                    word: word.toString(),
+                    solution: solutions[index].toString()
+                })
+            })
+        const requestData = {
+            name: name,
+            ownerId: 1,
+            cards: cards
+        }
 
-     }*/
+        console.log(requestData);
+
+        axios.post("/api/studyset/create", requestData)
+            .then(() => {
+                console.log("Success.");
+                navigate("/collections");
+            })
+            .catch(() => console.log("Kaputt."))
+    }
 
     const handleFillComplete = () => {
         console.log("Callback called.");
@@ -36,14 +60,14 @@ function AddStudyset() {
                 <Link to={"/collections"}>Back</Link>
             </div>
             <div className={"h1"}>Add Studyset</div>
-            <form>
+            <form onSubmit={handleSubmit} autoComplete="off">
                 <div className={"studyset-name"}>
                     <label htmlFor="name">Name:</label>
-                    <input placeholder={"Studyset name"} value={name} type={"text"} onChange={(e) => {
+                    <input name="name" placeholder={"Studyset name"} value={name} type={"text"} onChange={(e) => {
                         setName(e.target.value);
                     }}/>
                 </div>
-                {fields.map((field, index) => (
+                {fields.map((_field, index) => (
                     <CardComponent key={index} callback={handleFillComplete}/>
                 ))}
                 <div className={"submit-button"}>
@@ -56,109 +80,3 @@ function AddStudyset() {
 }
 
 export default AddStudyset
-
-
-/*
-
-import React, {useState} from "react";
-import axios from "axios";
-import {Link, useNavigate} from "react-router-dom";
-import "./AddStudyset.css";
-
-interface Field {
-    word: string;
-    solution: string;
-
-    [key: string]: string; // Index signature
-}
-
-function AddStudyset() {
-    const navigate = useNavigate();
-    const [name, setName] = useState<string>("");
-    const [fields, setFields] = useState<Field[]>([{id: 1, word: '', solution: ''}]);
-
-    const handleChange = (index: number, key: string, value: string) => {
-        const newFields = [...fields];
-        newFields[index][key] = value;
-        setFields(newFields);
-    };
-
-    const handleAddField = () => {
-        const newFields = [...fields];
-        newFields.push({id: fields.length + 1, word: '', solution: ''});
-        setFields(newFields);
-    };
-
-    const handleRemoveField = (index: number) => {
-        const newFields = [...fields];
-        newFields.splice(index, 1);
-        setFields(newFields);
-    };
-
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (!name || fields.some(field => !field.word || !field.solution)) {
-            throw new Error("All fields must be filled");
-        }
-
-        const studysetData = {
-            name: name,
-            words: fields.map(field => ({word: field.word, solution: field.solution}))
-        };
-
-        axios.post("/studyset/create", studysetData).then(res => {
-            console.log(res);
-            navigate("/collections");
-        });
-    };
-
-    return (
-        <>
-            <div className={"align-left"}>
-                <Link to={"/collections"}>Back</Link>
-            </div>
-            <div className={"h1"}>Add Studyset</div>
-            <form onSubmit={handleSubmit}>
-                <div className={"studyset-name"}>
-                    <label htmlFor="name">Name:</label>
-                    <input placeholder={"Studyset name"} value={name} type={"text"} onChange={(e) => {
-                        setName(e.target.value);
-                    }}/>
-                </div>
-                <div className={"dynamic-form"}>
-                    {fields.map((field, index) => (
-                        <div key={field.id}>
-                            <label htmlFor={"word"}>Word:</label>
-                            <input
-                                id={"word"}
-                                placeholder={"Enter word"}
-                                value={field.word}
-                                onChange={(e) => handleChange(index, 'word', e.target.value)}
-                            />
-                            <label htmlFor={"solution"}>Solution:</label>
-                            <input
-                                id={`solution-${index}`}
-                                placeholder={"Enter solution"}
-                                value={field.solution}
-                                onChange={(e) => handleChange(index, 'solution', e.target.value)}
-                            />
-                            <button type="button" onClick={() => handleRemoveField(index)}>
-                                Remove
-                            </button>
-                        </div>
-                    ))}
-                    <button type="button" onClick={handleAddField}>
-                        Add Field
-                    </button>
-                </div>
-                <div className={"submit-button"}>
-                    <button type={"submit"}>Add Studyset</button>
-                </div>
-            </form>
-        </>
-    );
-}
-
-export default AddStudyset;
-
-*/
--- frontend/src/addStudyset/CardComponent.tsx
+++ frontend/src/addStudyset/CardComponent.tsx
@@ -7,11 +7,13 @@ interface CardProps {
 export default function CardComponent(props: CardProps) {
     const [word, setWord] = useState<string>("");
     const [solution, setSolution] = useState<string>("");
+    const [children] = useState<number[]>([1]);
 
     const handleChangeWord = (e: React.FormEvent<HTMLInputElement>) => {
         const newWord = e.currentTarget.value;
         setWord(newWord);
-        if (newWord.length === 1 && solution.length >= 1) {
+        if ((newWord.length === 1 && solution.length >= 1) && children.length > 0) {
+            children.pop();
             props.callback();
         }
     }
@@ -19,7 +21,8 @@ export default function CardComponent(props: CardProps) {
     const handleChangeSolution = (e: React.FormEvent<HTMLInputElement>) => {
         const newSolution = e.currentTarget.value;
         setSolution(newSolution);
-        if (newSolution.length === 1 && word.length >= 1) {
+        if ((newSolution.length === 1 && word.length >= 1) && children.length > 0) {
+            children.pop();
             props.callback();
         }
     }

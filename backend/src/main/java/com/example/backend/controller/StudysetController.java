30a47eb7f2eefd1bffed0fd0a929aad0b2af9520
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2024-05-15 13:59:08 +0200

    added "/api" to path

--- backend/src/main/java/com/example/backend/controller/StudysetController.java
+++ backend/src/main/java/com/example/backend/controller/StudysetController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/studyset")
+@RequestMapping("/api/studyset")
 public class StudysetController {
 
     private final StudysetService studysetService;

d1e5bf83b58b7b24112f4bb045d1c4e04e3319f6
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2024-06-04 16:25:41 +0200

    adjusted code in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
-import Login from "./Login/Login.tsx";
+import Login from "./login/Login.tsx";
 import Collections from "./collections/Collections.tsx";
 import AddStudyset from "./addStudyset/AddStudyset.tsx";
 import LearnStudyset from "./learnStudyset/LearnStudyset.tsx";
+import Writing from "./Writing/Writing.tsx";
+
+import Flashcards from "./flashcard/Flashcards.tsx";
 
 function App() {
 
@@ -11,9 +14,11 @@ function App() {
 
         <Routes>
             <Route path="/login" element={<Login/>}/>
-            <Route path="/collections" element={<Collections/>}/>
-            <Route path="/add+studyset" element={<AddStudyset/>}/>
-            <Route path="/learn/{name}" element={<LearnStudyset/>}/>
+            <Route path="/" element={<Collections/>}/>
+            <Route path="/add-studyset" element={<AddStudyset/>}/>
+            <Route path="/learn/:name" element={<LearnStudyset/>}/>
+            <Route path="/learn/:name/writing" element={<Writing/>}/>
+            <Route path="/learn/:name/flashcards" element={<Flashcards/>}/>
         </Routes>
 
     )

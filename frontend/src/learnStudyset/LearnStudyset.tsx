84f6708428ea153d9502f2cb5de9337f6acdfc8d
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2024-06-04 16:27:22 +0200

    changed code  in LearnStudyset

--- frontend/src/learnStudyset/LearnStudyset.tsx
+++ frontend/src/learnStudyset/LearnStudyset.tsx
@@ -1,4 +1,26 @@
-export default function LearnStudyset() {
-    return null;
+import {Link, useNavigate, useParams} from "react-router-dom";
+import './LearnStudyset.css'
 
+export default function LearnStudyset() {
+    const params = useParams();
+    const name: string | undefined = params.name;
+    const navigate = useNavigate();
+
+
+    return (
+        <>
+            <div className="back">
+                <Link to={"/"}>Back</Link>
+            </div>
+            <div className="h1">{name}</div>
+            <div className={"flex-row"}>
+                <div className="collection-item" onClick={() => navigate(`/learn/${name}/flashcards`)}>
+                    <span>Flashcards</span>
+                </div>
+                <div className="collection-item" onClick={() => navigate(`/learn/${name}/writing`)}>
+                    <span>Writing</span>
+                </div>
+            </div>
+        </>
+    )
 }

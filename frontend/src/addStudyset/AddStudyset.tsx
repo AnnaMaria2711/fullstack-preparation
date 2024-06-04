628d9825b30392514fba7dae131979e1460db809
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2024-06-04 16:22:58 +0200

    created addstudyset page to add word collections

--- frontend/src/addStudyset/AddStudyset.tsx
+++ frontend/src/addStudyset/AddStudyset.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Link, useNavigate} from "react-router-dom";
 import "./AddStudyset.css";
 import CardComponent from "./CardComponent.tsx";
@@ -8,35 +8,21 @@ import {Card} from "../types.tsx";
 
 function AddStudyset() {
 
+    const emptyCard: Card = {word: "", solution: ""};
     const navigate = useNavigate();
     const [name, setName] = useState<string>("");
-    const [cards] = useState<Card[]>([]);
-    const [fields, setFields] = useState<number[]>([1]);
+    const [cards, setCards] = useState<Card[]>([emptyCard]);
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if (!fields) {
-            throw new Error("Input fields must be filled");
+        console.log(cards);
+        if (cards.length === 1 && (cards[0].word === "" || cards[0].solution === "")) {
+            console.warn("No completed cards");
         }
-        console.log(e.currentTarget);
-        const formData = new FormData(e.currentTarget);
-        console.log(formData);
-        console.log(formData.get("name"))
-        console.log(formData.getAll("word"))
-        console.log(formData.getAll("solution"))
-        const words = formData.getAll("word");
-        const solutions = formData.getAll("solution");
-        words.filter((word, index) => word !== "" && solutions[index] !== "")
-            .map((word, index) => {
-                cards.push({
-                    word: word.toString(),
-                    solution: solutions[index].toString()
-                })
-            })
         const requestData = {
             name: name,
             ownerId: 1,
-            cards: cards
+            cards: [...cards].splice(0, cards.length - 1)
         }
 
         console.log(requestData);
@@ -44,34 +30,82 @@ function AddStudyset() {
         axios.post("/api/studyset/create", requestData)
             .then(() => {
                 console.log("Success.");
-                navigate("/collections");
+                navigate("/");
             })
             .catch(() => console.log("Kaputt."))
     }
 
-    const handleFillComplete = () => {
-        console.log("Callback called.");
-        setFields(prevField => [...prevField, 1])
+    const removeAtIndex = (index: number) => {
+        const newCards: Card[] = [];
+        cards.forEach((c, i) => {
+            if (i !== index) newCards.push(c);
+        })
+        return newCards;
+    }
+
+    useEffect(() => {
+        let i: number = 0;
+        let oneEmpty: boolean = false;
+        while (i < cards.length && !oneEmpty) {
+            oneEmpty = cards[i].solution == "" || cards[i].word == "";
+            i++;
+        }
+        if (!oneEmpty) {
+            setCards(current => [...current, {...emptyCard}])
+        }
+    }, [cards]);
+
+    const setWord = (index: number, w: string) => {
+        setCards(current => {
+            return current.map((c, i) => ({
+                ...c
+                , word: (i === index) ? w : c.word
+            }));
+        });
+    }
+
+    const setSolution = (index: number, s: string) => {
+        setCards(current => {
+            return current.map((c, i) => ({
+                ...c,
+                solution: (i === index) ? s : c.solution
+            }));
+        });
+    }
+
+    const deleteCard = (cardIndex: number) => {
+        setCards(removeAtIndex(cardIndex));
     }
 
     return (
         <>
-            <div className={"align-left"}>
-                <Link to={"/collections"}>Back</Link>
+            <div className={"back"}>
+                <Link to={"/"}>Back</Link>
             </div>
             <div className={"h1"}>Add Studyset</div>
-            <form onSubmit={handleSubmit} autoComplete="off">
-                <div className={"studyset-name"}>
-                    <label htmlFor="name">Name:</label>
-                    <input name="name" placeholder={"Studyset name"} value={name} type={"text"} onChange={(e) => {
-                        setName(e.target.value);
-                    }}/>
-                </div>
-                {fields.map((_field, index) => (
-                    <CardComponent key={index} callback={handleFillComplete}/>
-                ))}
-                <div className={"submit-button"}>
-                    <button type={"submit"}> Add Studyset</button>
+            <form onSubmit={handleSubmit} autoComplete={"off"}>
+                <div className={"form-container"}>
+                    <div className={"studyset-name"}>
+                        <label htmlFor="name">Name:</label>
+                        <input name="name" placeholder={"Studyset name"} value={name} type={"text"}
+                               onChange={(e) => {
+                                   setName(e.target.value);
+                               }}/>
+                    </div>
+                    {cards.map((card, i) => {
+                        return <div key={i}>
+                            <CardComponent card={card} setSolution={(s) => {
+                                setSolution(i, s);
+                            }} setWord={(w) => {
+                                setWord(i, w)
+                            }} delete={() => {
+                                deleteCard(i);
+                            }} isLast={i === cards.length - 1}/>
+                        </div>
+                    })}
+                    <div className={"submit-button"}>
+                        <button type={"submit"}> Add Studyset</button>
+                    </div>
                 </div>
             </form>
 
